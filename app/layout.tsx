599fb9bad66a63ed8387c5154c0d66049bdee698
includeovalle <ovalle.carlosr@gmail.com>
2023-04-10 22:42:10 -0600

    feature: add global fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,38 @@
 import './styles/globals.scss';
+import Image from 'next/image';
+import { Roboto_Slab } from 'next/font/google'
+import BgImage from '../public/assets/horse-neb.jpg';
 
+const roboto_slab = Roboto_Slab({
+  subsets: ['latin'],
+  variable: '--font-roboto-slab',
+  weight: '400',
+  display: 'swap',
+});
+//const stick_no_bills = Stick_No_Bills({
+//  subsets: ['latin'],
+//  variable: '--font-stick-no-bills',
+//  weight: '400',
+//  display: 'swap',
+//});
 export default function RootLayout({ children }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html>
+      <body className={roboto_slab.className} >
+        <Image src={BgImage} alt="Horse Nebula" fill
+          sizes="(max-width: 576px) 100vw, 576px"
+          style={{ objectFit: 'cover', objectPosition: 'center' }}
+          priority={true}
+          quality={100}
+        />
+        {children}
+      </body>
     </html>
   );
 }
 export const metadata = {
-  title: 'Bienvenido viajero',
+  title: 'MIW: Bienvenido viajero',
   description: 'Un sitio para viajeros',
 }
