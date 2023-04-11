56f3d21f7ed14882c7c9f26d0478715e41e1e343
includeovalle <ovalle.carlosr@gmail.com>
2023-04-10 22:39:09 -0600

    label component and story add

--- stories/atoms/label/Label.stories.ts
+++ stories/atoms/label/Label.stories.ts
@@ -4,13 +4,34 @@ import { Label } from '../../../app/components/atoms/label/Label';
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
 const meta: Meta<typeof Label> = {
-  title: 'Components/Atoms/Label',
+  title: 'Components/Atoms/label/Label',
   component: Label,
   tags: ['autodocs'],
   argTypes: {
-    className: {
+    label: {
+      table: {
+        description: 'className',
+
+        type: {
+          summary: 'string',
+        },
+        defaultValue: {
+          summary: 'empty string',
+        },
+      },
+    }
+    ,className: {
       control: 'select',
-        options:[ 'default'],
+      options: ['default'],
+      //set  default value for className to(storybook 7):  'default'
+      table: {
+        type: {
+          summary: 'string',
+        },
+        defaultValue: {
+          summary: 'default',
+        },
+      },
     },
   },
 };
@@ -21,6 +42,6 @@ type Story = StoryObj<typeof Label>;
 // More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
 export const Primary: Story = {
   args: {
-    label: 'label bien',
+    label: 'label story',
   },
 };
