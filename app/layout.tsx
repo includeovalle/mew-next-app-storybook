100111b8a062f15dafec81935e4666ec0992c814
includeovalle <ovalle.carlosr@gmail.com>
2023-04-15 13:21:49 -0600

    feture: update background remove global bg style

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({ children }: {
       <body className={roboto_slab.className} >
         <Image src={BgImage} alt="Horse Nebula" fill
           sizes="(max-width: 576px) 100vw, 576px"
-          style={{ objectFit: 'cover', objectPosition: 'center' }}
-          priority={true}
+          style={{ objectFit: 'cover', objectPosition: 'center',background: 'rgb(222,0,255)',background: 'radial-gradient(circle, rgba(222,0,255,0.9724483543417367) 6%, rgba(46,1,1,1) 59%)', zIndex:-1 }}
+          priority
           quality={100}
         />
         {children}
