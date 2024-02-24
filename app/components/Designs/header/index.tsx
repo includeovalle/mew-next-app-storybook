20a129e6c006e36f36afd287b486071e2c058e7f
includeovalle <ovalle.carlosr@gmail.com>
2024-02-24 08:47:12 -0600

    chore: [header] add header layout

--- app/components/Designs/header/index.tsx
+++ app/components/Designs/header/index.tsx
@@ -1,21 +1,36 @@
-import { Header, CustomImage, Navbar } from '@/app/components/';
-import logo from '@/public/logo.png';
 import React from "react";
+import { Header, CustomImage, Navbar, Container } from '@/app/components/';
+import logo from '@/public/logo.png';
 
 const Index = () => {
 
     const webMenu = [
-        ]
+    ]
     const mobileMenu = [
-]
+    ]
 
-    const href =  "/";
-    const alt =  "soultech inicio";
+    const href = "/";
+    const alt = "mew game logo";
 
     return (
 
-        <Header className={'header__two-items'} >
+        <Header className='header' >
             <CustomImage links href={href} className={'logo'} src={logo} alt={alt} />
+            <Container className="UserMenu">
+                <Container className="recursos">
+        <div>100000000</div>
+        <div>100000000</div>
+        <div>100000000</div>
+        <div>100000000</div>
+                </Container >
+                <Container className="configuracion-usuario">
+                    user config
+                </Container >
+                <Container className="global-menu">
+                    global
+                </Container >
+            </Container >
+
         </Header>
     );
 };
