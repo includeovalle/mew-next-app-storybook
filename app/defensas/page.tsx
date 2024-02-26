0cb1c0552f5477c1f5d722c7e0cd6327b17e9c77
includeovalle <ovalle.carlosr@gmail.com>
2024-02-25 21:58:49 -0600

    update:[left-menu] create routes for left menu

---
+++ app/defensas/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {LeftMenu } from "@/app/components/Designs/";
+import { InnerContainer, Container } from "@/app/components/";
+
+const Index = () => {
+
+  return (
+      <InnerContainer>
+        <LeftMenu />
+      <Container  className="vision-general">
+        planeta
+        ao
+      </Container>
+
+      </InnerContainer>
+  );
+};
+
+export default Index;
+
