bba2b538a6d03b62de32bb7c9de261616bbe5ccc
includeovalle <ovalle.carlosr@gmail.com>
2024-03-02 22:04:53 -0600

    begin planeta menus

--- app/planeta/page.tsx
+++ app/planeta/page.tsx
@@ -1,18 +1,33 @@
 import React from "react";
-import {LeftMenu } from "@/app/components/Designs/";
-import { InnerContainer, Container } from "@/app/components/";
+import { LeftMenu } from "@/app/components/Designs/";
+import { InnerContainer, Container, CustomImage } from "@/app/components/";
 
 const Index = () => {
 
   return (
-      <InnerContainer>
-        <LeftMenu />
-      <Container  className="vision-general">
-        planeta
-        ao
+    <InnerContainer>
+      <LeftMenu />
+      <Container className="vision-general">
+        <Container className="movimiento-flotas">
+          <div><p> Una flota procedente de 3:18 se dirige a planeta: 5.11</p></div>
+        </Container>
+        <Container className="lista_planetas">
+          <div>
+            <CustomImage figcaption > alo </CustomImage  >
+          </div>
+          <div>
+            <div>luni</div>
+          </div>
+        </Container>
+        <Container className="info_planetas">
+          <div>
+            foto aca
+          </div>
+
+        </Container>
       </Container>
 
-      </InnerContainer>
+    </InnerContainer>
   );
 };
 
